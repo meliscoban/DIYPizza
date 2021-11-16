b6a54230c8d2277ffa448d722fc8bbc20b8aa954
Melis Çoban <melis.coban5@gmail.com>
2021-11-16 12:02:59 +0300

    malzeme seçimi

--- DIYPizza/Controllers/HomeController.cs
+++ DIYPizza/Controllers/HomeController.cs
@@ -1,26 +1,57 @@
 ﻿using DIYPizza.Models;
+using DIYPizza.Models.Data;
+using DIYPizza.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace DIYPizza.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly DatabaseContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(DatabaseContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            List<Secim> secimler = _context.Malzemeler.Select(m => new Secim() { MalzemeId = m.Id, MalzemeAd = m.Ad, SeciliMi = false }).ToList();
+
+            return View(secimler);
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Index(List<Secim> secimler)
+        {
+            if (secimler.Count(x => x.SeciliMi) < 2)
+            {
+                ModelState.AddModelError("", "En az 2 adet malzeme seçmeniz gerekmektedir.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                var secilenler = secimler.Where(x => x.SeciliMi).ToList();
+                TempData["SecilenMalzemeler"] = JsonSerializer.Serialize(secilenler);
+                return RedirectToAction("Siparis");
+            }
+
+            return View(secimler);
+        }
+
+        public IActionResult Siparis()
+        {
+            //string json = (string)TempData["SecilenMalzemeler"];
+            //List<Secim> secimler = JsonSerializer.Deserialize<List<Secim>>(json);
+            List<Secim> secimler = JsonSerializer.Deserialize<List<Secim>>((string)TempData["SecilenMalzemeler"]);
+            return View(secimler);
         }
 
         public IActionResult Privacy()
--- DIYPizza/Models/Data/DatabaseContext.cs
+++ DIYPizza/Models/Data/DatabaseContext.cs
@@ -13,6 +13,34 @@ namespace DIYPizza.Models.Data
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Malzeme>().HasData(
+                new Malzeme() { Id = 1, Ad = "Mozzarella" },
+                new Malzeme() { Id = 2, Ad = "Pizza Sosu" },
+                new Malzeme() { Id = 3, Ad = "Mısır" },
+                new Malzeme() { Id = 4, Ad = "Zeytin" },
+                new Malzeme() { Id = 5, Ad = "Salam" },
+                new Malzeme() { Id = 6, Ad = "Sucuk" },
+                new Malzeme() { Id = 7, Ad = "Sosis" },
+                new Malzeme() { Id = 8, Ad = "Biber" },
+                new Malzeme() { Id = 9, Ad = "Mantar" },
+                new Malzeme() { Id = 10, Ad = "Pastırma" },
+                new Malzeme() { Id = 11, Ad = "Ananas" },
+                new Malzeme() { Id = 12, Ad = "Ton Balığı" },
+                new Malzeme() { Id = 13, Ad = "Rokfor" },
+                new Malzeme() { Id = 14, Ad = "Kaşar" },
+                new Malzeme() { Id = 15, Ad = "Kavurma" },
+                new Malzeme() { Id = 16, Ad = "Füme" },
+                new Malzeme() { Id = 17, Ad = "Soğan" },
+                new Malzeme() { Id = 18, Ad = "Tavuk" },
+                new Malzeme() { Id = 19, Ad = "Parmesan" },
+                new Malzeme() { Id = 20, Ad = "Domates" }
+                );
+        }
+
         public DbSet<Malzeme> Malzemeler { get; set; }
     }
 }
