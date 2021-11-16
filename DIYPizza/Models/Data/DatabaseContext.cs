767ec36e15a730b7a05234252bad90f2bed9c35a
Melis Çoban <melis.coban5@gmail.com>
2021-11-16 10:55:37 +0300

    Add project files.

---
+++ DIYPizza/Models/Data/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using DIYPizza.Models.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DIYPizza.Models.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Malzeme> Malzemeler { get; set; }
+    }
+}
---
+++ DIYPizza/Models/Entity/Malzeme.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DIYPizza.Models.Entity
+{
+    public class Malzeme
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(20)]
+        public string Ad { get; set; }
+    }
+}
